29b8d708eaa71c958336ed8c34fa722f4c6e5b1e
Kleinbard <kkleinbard@avaya.com>
2024-09-06 12:18:20 -0400

    new

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//   base: './',
+//   build: {
+//     outDir: 'dist',
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'dist',
-  }
+  base: '/vom_dash/',
+  // other configurations...
 })
